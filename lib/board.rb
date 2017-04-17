b2a237dcc5f5882764c7ec7a89f9c3d0fd4c411f
david <acuffdavid@gmail.com>
2017-04-17 11:42:25 -0400

    add board output and updating

--- lib/board.rb
+++ lib/board.rb
@@ -1,27 +1,37 @@
-# set up board to initial state
-# ask for input (two separate things)
-	# move which piece?
-	# move where?
-
-# maybe implement rows and columns as 1-8 A-H, which would return a number 1-64 (1d array seems easier)
-
 class Board
 	attr_accessor :board
 
 	def initialize
-		@board = [R,N,B,Q,K,B,N,R,
-  				  P,P,P,P,P,P,P,P,
-  				  _,_,_,_,_,_,_,_,
-  				  _,_,_,_,_,_,_,_,
-  				  _,_,_,_,_,_,_,_,
-  				  _,_,_,_,_,_,_,_,
-  				  P,P,P,P,P,P,P,P,
-  				  R,N,B,Q,K,B,N,R]
+		@board = ["R","N","B","K","Q","B","K","R",
+				  "P","P","P","P","P","P","P","P",
+				  "_","_","_","_","_","_","_","_",
+				  "_","_","_","_","_","_","_","_",
+				  "_","_","_","_","_","_","_","_",
+				  "_","_","_","_","_","_","_","_",
+				  "P","P","P","P","P","P","P","P",
+				  "R","N","B","K","Q","B","K","R"]
 	end
 
 	def convertRankFile ()
 	end
 
+	def isLegal
+	end
+
 	def updateBoard (from, to)
+		temp = @board[from]
+		@board[from] = "_"
+		@board[to] = temp
+	end
+
+	def display
+		puts @board[0..7].join(" ")
+		puts @board[8..15].join(" ")
+		puts @board[16..23].join(" ")
+		puts @board[24..31].join(" ")
+		puts @board[32..39].join(" ")
+		puts @board[40..47].join(" ")
+		puts @board[48..55].join(" ")
+		puts @board[56..63].join(" ")
 	end
 end
