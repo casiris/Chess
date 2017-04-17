e50eb400cf752af255775affda0d80fdc16a8789
david <acuffdavid@gmail.com>
2017-04-17 11:41:31 -0400

    very basic input

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,15 @@
-class Game
+require_relative "board"
+
+def validateInput ()
 end
+
+b = Board.new
+b.display
+
+puts "Enter a piece to move"
+from = gets.chomp
+puts "Enter where to move"
+to = gets.chomp
+
+b.updateBoard(from.to_i,to.to_i)
+b.display
