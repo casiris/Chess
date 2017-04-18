a832912e30da3c484eda5a80177fb2e28c29c8a0
david <acuffdavid@gmail.com>
2017-04-18 12:39:21 -0400

    basic legality checking

---
+++ lib/pawn.rb
@@ -0,0 +1,31 @@
+class Pawn
+	def isLegal (from,to,black=false)
+		if (black)
+			if (from.between?(8,15))
+				if ((from-to).abs == 8 || (from-to).abs == 16)
+					return true
+				else
+					return false
+				end
+			end
+		else
+			if (from.between?(48,55))
+				if ((from-to).abs == 8 || (from-to).abs == 16)
+					return true
+				else
+					return false
+				end
+			end
+		end
+	end
+
+	def capture ()
+	end
+end
+
+
+# need to check starting position to determine if it can move 2 spaces or just one
+# need to move a white pawn and a black pawn "forward" relatively
+	# so white would subtract index to go "up" the board, and black would add
+
+# will need en passent and promotion
