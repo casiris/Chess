e4b6a0a56f61c398fc20c1449e18803bd844d21a
david <acuffdavid@gmail.com>
2017-04-26 12:36:08 -0400

    moved initialization of owned pieces from game to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,14 @@
 class Player
-	attr_accessor :pieces
+	attr_accessor :name, :pieces
 
-	def initialize
+	def initialize (name,position)
+		@name = name
 		@pieces = []
+		position.each do |i|
+			@pieces << i
+		end
+	end
+
+	def updatePiece(from,to)
 	end
 end
