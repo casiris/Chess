b91a565bf0d4f4410a8f9832b161483b1fb40490
david <acuffdavid@gmail.com>
2017-04-30 12:45:30 -0400

    can now move rooks

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require_relative "board"
 require_relative "player"
 require_relative "pawn"
+require_relative "rook"
 
 class Game
     #attr_accessor :board, :coordinate
@@ -10,6 +11,7 @@ class Game
         @playerWhite = Player.new("White","A".ord.."Z".ord)     # ascii code of upper and lower case numbers
         @playerBlack = Player.new("Black","a".ord.."z".ord)
         @pawn = Pawn.new
+        @rook = Rook.new
         @coordinate = [ "a1","b1","c1","d1","e1","f1","g1","h1",
                         "a2","b2","c2","d2","e2","f2","g2","h2",
                         "a3","b3","c3","d3","e3","f3","g3","h3",
@@ -76,6 +78,10 @@ class Game
             else
                 @pawn.capture(from,to,false)
             end
+        when "r"
+            @rook.isLegal(from,to)
+        when "R"
+            @rook.isLegal(from,to)
         else
         end
     end
@@ -119,31 +125,9 @@ end
 g = Game.new
 g.gameLoop
 
-# get input (what piece do you want to move?)
-    # validate it
-        # convert the rank and file to a number, return it
-    # check board at that index
-        # if it's empty (_), print out error and ask for another position
-        # if it isn't, return that piece
-# get second input (where to move to)
-    # validate and convert the rank/file
-    # call respective function for whatever piece was returned prior
-        # check "to" for legality
-            # if it isn't legal, ask for input again
-            # if it is, update the board
-            # switch player
-
 
 # for check/mate, and i guess in the future for ai, i'll need a function to check what threatens a given square
 # if i'm not mistaken, i'd just have to check in all 8 directions, and see if there's any applicable piece
 # in that direction, ie, is there anything on any diagonal, and if so, is it a pawn 1 space away, or a bishop/queen
 # any distance away, etc
 # so, kinda like checking for connect four
-
-
-# white moves
-# make sure white can only move white pieces
-    # i guess instead of keeping track of indicies, i could use ascii codes to determine which piece belongs to who
-    # like, if the ascii code is between x-y it's lowercase, and x1-y1 is upppercase
-# if a player moves a piece to a square that's not empty, check if it's an opposing piece
-# if it is, call the capture function on the piece that was moved
