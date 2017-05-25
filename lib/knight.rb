7f1e6d787d66d495907f3357ce12cdce27b53045
david <acuffdavid@gmail.com>
2017-05-25 00:58:11 -0400

    starting fresh to do it right this time

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,16 +1,3 @@
 class Knight
-	def isLegal (from,to)
-		# can only move +-1/+-2 or +-2/+-1
-		# so if change in x is 1, then change in y has to be 2, or vice versa
-		# if it's anything else, knight can't move there
-		fromX = from / 8
-		fromY =  from % 8
-		toX = to / 8
-		toY = to % 8
-
-		if (((toX-fromX).abs == 1 && (toY-fromY).abs == 2) || ((toX-fromX).abs == 2 && (toY-fromY).abs == 1))
-			return true
-		end
-		return false
-	end
+	
 end
