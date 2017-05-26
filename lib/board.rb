e8e7c53b1237e31b2302e59f9bc8154de5f0341e
david <acuffdavid@gmail.com>
2017-05-26 13:28:03 -0400

    finished setting up board's initial state

--- lib/board.rb
+++ lib/board.rb
@@ -9,22 +9,44 @@ class Board
     attr_accessor :board
 
     def initialize
-    	pawn_black = Pawn.new("Black")
-        @board = [[],
-        		  [pawn_black,pawn_black,pawn_black,pawn_black,pawn_black,pawn_black,pawn_black,pawn_black],
+    	bPawn = Pawn.new("Black","\u2659")
+        bRook = Rook.new("Black","\u2656")
+        bKnight = Knight.new("Black","\u2658")
+        bBishop = Bishop.new("Black","\u2657")
+        bQueen = Queen.new("Black","\u2655")
+        bKing = King.new("Black","\u2654")
+        wPawn = Pawn.new("White","\u265F")
+        wRook = Rook.new("White","\u265C")
+        wKnight = Knight.new("White","\u265E")
+        wBishop = Bishop.new("White","\u265D")
+        wQueen = Queen.new("White","\u265A")
+        wKing = King.new("White","\u265B")
+        @board = [[bRook,bKnight,bBishop,bQueen,bKing,bBishop,bKnight,bRook],
+        		  [bPawn,bPawn,bPawn,bPawn,bPawn,bPawn,bPawn,bPawn],
         		  [],
         		  [],
         		  [],
         		  [],
-        		  [],
-        		  []]
+        		  [wPawn,wPawn,wPawn,wPawn,wPawn,wPawn,wPawn,wPawn],
+        		  [wRook,wKnight,wBishop,wQueen,wKing,wBishop,wKnight,wRook]]
     end
 
     def pieceAtIndex (x,y)
         # call the piece's toString method to output its name/color
-        return @board[x][y].toString
+        if (@board[x][y] != nil)
+            return @board[x][y].toString
+        else
+            return "No piece at that position"
+        end
+    end
+
+    def display
+        puts @board[0][1].unicode
+        # can't puts unicode of a range (needs to be an actual method)
+        # but i probably want to do it a different way anyway, because the middle of the board is nil
     end
 end
 
 b = Board.new
-puts b.pieceAtIndex(1,2)
+puts b.pieceAtIndex(1,0)
+b.display
