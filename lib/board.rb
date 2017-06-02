b441f76cf79fde55657c6a4f249fa24aa6fd179b
david <acuffdavid@gmail.com>
2017-06-02 12:34:01 -0400

    fixed update's x/y being reversed

--- lib/board.rb
+++ lib/board.rb
@@ -23,31 +23,39 @@ class Board
         wKing = King.new("White","\u265B")
         @board = [[bRook,bKnight,bBishop,bQueen,bKing,bBishop,bKnight,bRook],
         		  [bPawn,bPawn,bPawn,bPawn,bPawn,bPawn,bPawn,bPawn],
-        		  [],
-        		  [],
-        		  [],
-        		  [],
+        		  [nil,nil,nil,nil,nil,nil,nil,nil],
+        		  [nil,nil,nil,nil,nil,nil,nil,nil],
+        		  [nil,nil,nil,nil,nil,nil,nil,nil],
+        		  [nil,nil,nil,nil,nil,nil,nil,nil],
         		  [wPawn,wPawn,wPawn,wPawn,wPawn,wPawn,wPawn,wPawn],
         		  [wRook,wKnight,wBishop,wQueen,wKing,wBishop,wKnight,wRook]]
         @file = ["a","b","c","d","e","f","g","h"]
     end
 
-    def pieceAtIndex (x,y)
-        # call the piece's toString method to output its name/color
+    def pieceAtIndex (position)
+        # do math to get the x/y position from the given 0-63 index
+        x = position / 8
+        y = position % 8
         if (@board[x][y] != nil)
-            return @board[x][y].toString
+            return @board[x][y]
         else
-            return "No piece at that position"
+            return nil
         end
     end
 
-    def update (fromX,fromY,toX,toY)
-        temp = @board[fromY][fromX]
-        @board[fromY][fromX] = nil
-        @board[toY][toX] = temp
+    def update (from,to)
+        fromX = from / 8
+        fromY = from % 8
+        toX = to / 8
+        toY = to % 8
+        
+        temp = @board[fromX][fromY]
+        @board[fromX][fromY] = nil
+        @board[toX][toY] = temp
     end
 
     def display
+        puts ""
         puts "    #{@file.join(" ")}"     # print file
         puts ""
         for x in 0..7
@@ -61,11 +69,8 @@ class Board
             end
             puts ""
         end
+        puts ""
     end
 end
 
-b = Board.new
-puts b.pieceAtIndex(1,0)
-b.display
-b.update(0,1,0,3)
-b.display
+# update is kinda messy. it'd be nice to have just from/to. could do it with an array from[0]/from[1], or do math, or something else maybe
