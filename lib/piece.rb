9d12b4625341eec0bd3d89adcbad3dd4b5b3d7b7
david <acuffdavid@gmail.com>
2017-06-03 12:31:57 -0400

    setup addToPath and isLegal

--- lib/piece.rb
+++ lib/piece.rb
@@ -3,11 +3,24 @@ class Piece
 
 	def initialize (color,unicode)
 		@color = color
-		# @type = type
-		# @unicode = unicode
+		@path = []
 	end
 
 	def toString
 		"#{@color} #{@type}"
+	end
+
+	def addToPath
+
+	end
+
+	def isLegal (from,to)
+		return true		# just a placeholder
+
+		# i guess what i should do is have another function that adds pieces to a path
+		# and this function loops through and checks if there's an obstruction
+
+		# the addToPath function would add differently depending on the type of piece. rooks would go up down left right, bishop on the diagonals, etc
+		# and then a different function could check that path to see if the king is in the way, and if there are no obstructions, that'd be check
 	end
 end
