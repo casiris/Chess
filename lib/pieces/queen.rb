a6d9f2cbaa201b12138875a79700f7945b484111
david <acuffdavid@gmail.com>
2017-06-22 12:43:55 -0400

    moved piece classes into separate folder

---
+++ lib/pieces/queen.rb
@@ -0,0 +1,79 @@
+require_relative "../piece"
+
+class Queen < Piece
+	def initialize (color,unicode)
+		super
+		@type = "Queen"
+		@unicode = unicode
+		@diagonal = false
+	end
+
+	# constrain to cardinal directions or diagonals
+	def validateMove (from,to)
+		fromX = from / 8
+		fromY = from % 8
+		toX = to / 8
+		toY = to % 8
+
+		if (toX-fromX != 0 && toY-fromY == 0)
+			@diagonal = false
+			return true
+		elsif (toX-fromX == 0 && toY-fromY != 0)
+			@diagonal = false
+			return true
+		elsif ((toX-fromX).abs == (toY-fromY).abs)
+			@diagonal = true
+			return true
+		else
+			return false
+		end		
+	end
+
+	def movePath (from,to)
+		fromX = from / 8
+		fromY = from % 8
+		toX = to / 8
+		toY = to % 8
+		dirX = toX - fromX
+		dirY = toY - fromY
+
+		if (@diagonal == false)
+			if (fromX > toX)
+				north(fromX,fromY,toX)
+			elsif (fromX < toX)
+				south(fromX,fromY,toX)
+			end
+			if (fromY < toY)
+				east(fromX,fromY,toY)
+			elsif (fromY > toY)
+				west(fromX,fromY,toY)
+			end
+		else
+			if (dirX < 0 && dirY < 0)
+				northWest(fromX,fromY,toX,toY)
+			elsif (dirX < 0 && dirY > 0)
+				northEast(fromX,fromY,toX,toY)
+			elsif (dirY > 0 && dirY < 0)
+				southWest(fromX,fromY,toX,toY)
+			elsif (dirY > 0 && dirY > 0)
+				southEast(fromX,fromY,toX,toY)
+			end
+		end
+	end
+
+	def kingPath (pos)
+		posX = pos / 8
+		posY = pos % 8
+
+		north(posX,posY,0)
+		south(posX,posY,7)
+		east(posX,posY,7)
+		west(posX,posY,0)
+		northWest(posX,posY,0,0)
+		northEast(posX,posY,0,7)
+		southWest(posX,posY,7,0)
+		southEast(posX,posY,7,7)
+	end
+end
+
+# this is just an amalgamation of rook and bishop
---
+++ lib/pieces/rook.rb
@@ -0,0 +1,61 @@
+require_relative "../piece"
+
+class Rook < Piece
+	def initialize (color,unicode)
+		super
+		@type = "Rook"
+		@unicode = unicode
+	end
+
+	# constrain rook to cardinal directions only
+	def validateMove (from,to)
+		fromX = from / 8
+		fromY = from % 8
+		toX = to / 8
+		toY = to % 8
+
+		if (toX-fromX != 0 && toY-fromY == 0)
+			return true
+		elsif (toX-fromX == 0 && toY-fromY != 0)
+			return true
+		else
+			return false
+		end
+	end
+
+	def movePath (from,to)
+		fromX = from / 8
+		fromY = from % 8
+		toX = to / 8
+		toY = to % 8
+
+		# only want to call the direction "to" is in
+		# also want the loop bounds to only be from from-to, instead of the entire length of the board from the current position
+		# later, when checking for the king, i can go the entire distance in that direction
+		if (fromX > toX)
+			north(fromX,fromY,toX)
+		elsif (fromX < toX)
+			south(fromX,fromY,toX)
+		end
+		if (fromY < toY)
+			east(fromX,fromY,toY)
+		elsif (fromY > toY)
+			west(fromX,fromY,toY)
+		end
+	end
+
+	def kingPath (pos)
+		posX = pos / 8
+		posY = pos % 8
+
+		north(posX,posY,0)
+		south(posX,posY,7)
+		east(posX,posY,7)
+		west(posX,posY,0)
+	end
+end
+
+# rook can move anywhere, as long as the cardinal directions are clear
+# for instance, if moved out into the open middle, it can then move diagonally because there's no obstructions in the immediate NSEW
+# and bishop has been able to move wherever as well
+# so, movement isn't properly restrained
