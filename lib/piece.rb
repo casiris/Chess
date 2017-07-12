2df65daabc2770e4cb35c6ac370f2f2c8d1b8c6b
david <acuffdavid@gmail.com>
2017-07-12 12:19:12 -0400

    changed move generation to get index instead of actual object at index, also removed check functions because i need to rework them

--- lib/piece.rb
+++ lib/piece.rb
@@ -36,12 +36,19 @@ class Piece
 			end
 		end
 
-		return checkLastTileInPath(@path)
+		if (@path[0][-1] != nil)
+			xPos = @path[0][-1] / 8
+			yPos = @path[0][-1] % 8
+			piece = @board[xPos][yPos]
+			return checkLastTileInPath(piece)
+		else
+			return true
+		end
 	end
 
-	def checkLastTileInPath(path)
-		if (path[0][-1] != nil)
-			if (path[0][-1].color != self.color)
+	def checkLastTileInPath(piece)
+		if (piece != nil)
+			if (piece.color != self.color)
 				return true
 			else
 				return false
@@ -51,213 +58,7 @@ class Piece
 		end
 	end
 
-	# king will call this to check if it's in check
-	def check (from,board)
-		@board = board
-		fromX = from / 8
-		fromY = from % 8
-		@path = []
-
-		#puts self.toString		# prints correct piece, ie, differentiates between pieces
-
-		north(fromX,fromY,0)
-		south(fromX,fromY,7)
-		east(fromX,fromY,7)
-		west(fromX,fromY,0)
-		if (orthogonalCheck == true)
-			return true
-		end
-
-		# orthogonal only works when the king moves into check, not when a piece puts the king in check
-		# and that's because i'm calling it from the king in game
-		# i might need to call it from the last piece moved
-
-		northWest(fromX,fromY,0,0)
-		northEast(fromX,fromY,0,7)
-		if (northDiagonalCheck == true)
-			return true
-		end
-
-		southWest(fromX,fromY,7,0)
-		southEast(fromX,fromY,7,7)
-		if (southDiagonalCheck == true)
-			return true
-		end
-
-		if (knightCheck(from) == true)
-			return true
-		end
-	end
-
-	# check orthogonal directions for queen or rook
-	def orthogonalCheck
-		@path.each do |i|
-			i.each do |j|
-				if (j != nil)
-					if (j.color != self.color && (j.type == "Queen" || j.type == "Rook"))
-						return true
-					else	# if we hit a non-queen or non-rook, don't check further in that direction
-						break
-					end
-				end
-			end
-		end
-		@path = []
-		return false
-	end
-
-	# two different diagonal checks because black pawns can only threaten from the north, and white from the south
-	def northDiagonalCheck
-		@path.each do |i|
-			if (i[0] != nil)
-				# only check white king in north, because black king won't be checked by northern pawns (black pawns)
-				if ((i[0].type == "Pawn" && i[0].color == "Black") && self.color == "White")
-					return true
-				end
-			end
-			i.each do |j|
-				if (j != nil)
-					if (j.color != self.color && (j.type == "Queen" || j.type == "Bishop"))
-						return true
-					else
-						break
-					end
-				end
-			end
-		end
-		@path = []
-		return false
-	end
-
-	def southDiagonalCheck
-		@path.each do |i|
-			if (i[0] != nil)
-				if ((i[0].type == "Pawn" && i[0].color == "White") && self.color == "Black")
-					return true
-				end
-			end
-			i.each do |j|
-				if (j != nil)
-					if (j.color != self.color && (j.type == "Queen" || j.type == "Bishop"))
-						return true
-					else
-						break
-					end
-				end
-			end
-		end
-		@path = []
-		return false
-	end
-
 	# [-1,0]
-	def north (fromX,fromY,toX)
-		n = []
-
-		for i in toX..fromX-1
-			n << @board[i][fromY]
-		end
-
-		n.reverse!
-		@path << n
-	end
-
-	# [1,0]
-	def south (fromX,fromY,toX)
-		s = []
-
-		for i in fromX+1..toX
-			s << @board[i][fromY]
-		end
-
-		@path << s
-	end
-
-	# [0,1]
-	def east (fromX,fromY,toY)
-		e = []
-
-		for i in fromY+1..toY
-			e << @board[fromX][i]
-		end
-
-		@path << e
-	end
-
-	# [0,-1]
-	def west (fromX,fromY,toY)
-		w = []
-
-		for i in toY..fromY-1
-			w << @board[fromX][i]
-		end
-
-		w.reverse!
-		@path << w
-	end
-
-	# [-1,1]
-	def northEast (fromX,fromY,toX,toY)
-		ne = []
-		x = fromX-1
-		y = fromY+1
-
-		while (x >= toX && y <= toY)
-			ne << @board[x][y]
-			x -= 1
-			y += 1
-		end
-
-		@path << ne
-	end
-
-	# [-1,-1]
-	def northWest (fromX,fromY,toX,toY)
-		nw = []
-		x = fromX-1
-		y = fromY-1
-
-		while (x >= toX && y >= toY)
-			nw << @board[x][y]
-			x -= 1
-			y -= 1
-		end
-
-		@path << nw
-	end
-
-	# [1,1]
-	def southEast (fromX,fromY,toX,toY)
-		se = []
-		x = fromX+1
-		y = fromY+1
-
-		while (x <= toX && y <= toY)
-			se << @board[x][y]
-			x += 1
-			y += 1
-		end
-
-		@path << se
-	end
-
-	# [1,-1]
-	def southWest (fromX,fromY,toX,toY)
-		sw = []
-		x = fromX+1
-		y = fromY-1
-
-		while (x <= toX && y >= toY)
-			sw << @board[x][y]
-			x += 1
-			y -= 1
-		end
-
-		@path << sw
-	end
-
-	# something like this. can even rework the original directionals to work like this
-	# then treat the resulting path differently depending on checking for moving vs move generation
 	def northMoves (fromX,fromY,toX)
 		n = []
 
@@ -270,46 +71,98 @@ class Piece
 		@path << n
 	end
 
-	def knightCheck (pos)
-		kp = []
-		posX = pos / 8
-		posY = pos % 8
+	# [1,0]
+	def southMoves (fromX,fromY,toX)
+		s = []
 
-		# need to make sure we don't go out of bounds
-		if (posX+1 <= 7 && posY+2 <= 7)
-			kp << @board[posX+1][posY+2]
-		end
-		if (posX+1 <= 7 && posY-2 >= 0)
-			kp << @board[posX+1][posY-2]
-		end
-		if (posX-1 >= 0 && posY+2 <= 7)
-			kp << @board[posX-1][posY+2]
-		end
-		if (posX-1 >= 0 && posY-2 >= 0)
-			kp << @board[posX-1][posY-2]
-		end
-		if (posX+2 <= 7 && posY+1 <= 7)
-			kp << @board[posX+2][posY+1]
-		end
-		if (posX+2 <= 7 && posY-1 >= 0)
-			kp << @board[posX+2][posY-1]
-		end
-		if (posX-2 >= 0 && posY+1 <= 7)
-			kp << @board[posX-2][posY+1]
-		end
-		if (posX-2 >= 0 && posY-1 >= 0)
-			kp << @board[posX-2][posY-1]
+		for i in fromX+1..toX
+			s << i*8+fromY
 		end
 
-		kp.each do |i|
-			if (i != nil)
-				if (i.color != self.color && i.type == "Knight")
-					@path = []
-					return true
-				end
-			end
+		@path << s
+	end
+
+	# [0,1]
+	def eastMoves (fromX,fromY,toY)
+		e = []
+
+		for i in fromY+1..toY
+			e << fromX*8+i
 		end
-		return false
+
+		@path << e
+	end
+
+	# [0,-1]
+	def westMoves (fromX,fromY,toY)
+		w = []
+
+		for i in toY..fromY-1
+			w << fromX*8+i
+		end
+
+		w.reverse!
+		@path << w
+	end
+
+	# [-1,1]
+	def northEastMoves (fromX,fromY,toX,toY)
+		ne = []
+		x = fromX-1
+		y = fromY+1
+
+		while (x >= toX && y <= toY)
+			ne << x*8+y
+			x -= 1
+			y += 1
+		end
+
+		@path << ne
+	end
+
+	# [-1,-1]
+	def northWestMoves (fromX,fromY,toX,toY)
+		nw = []
+		x = fromX-1
+		y = fromY-1
+
+		while (x >= toX && y >= toY)
+			nw << x*8+y
+			x -= 1
+			y -= 1
+		end
+
+		@path << nw
+	end
+
+	# [1,1]
+	def southEastMoves (fromX,fromY,toX,toY)
+		se = []
+		x = fromX+1
+		y = fromY+1
+
+		while (x <= toX && y <= toY)
+			se << x*8+y
+			x += 1
+			y += 1
+		end
+
+		@path << se
+	end
+
+	# [1,-1]
+	def southWestMoves (fromX,fromY,toX,toY)
+		sw = []
+		x = fromX+1
+		y = fromY-1
+
+		while (x <= toX && y >= toY)
+			sw << x*8+y
+			x += 1
+			y -= 1
+		end
+
+		@path << sw
 	end
 end
 
