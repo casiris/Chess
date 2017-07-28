7c39be05dd6e466e02a07ab276a66d002541bd7d
david <acuffdavid@gmail.com>
2017-07-28 10:49:39 -0400

    added function to get position of en passant square

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,11 +1,11 @@
 require_relative "../piece"
 
 class Pawn < Piece
-
 	def initialize (color,unicode,pos)
 		super
 		@type = "Pawn"
 		@unicode = unicode
+		@enPassant = false
 	end
 
 	def validateMove (from,to)
@@ -23,11 +23,21 @@ class Pawn < Piece
 		toY = to % 8
 
 		if (@board[toX][toY] == nil)
-			if (fromX == 1 || fromX == 6) 		# if pawns are in respective home rows, ie, haven't yet moved
-				if (self.color == "Black" && (toX-fromX == 1 || toX-fromX == 2))
-					return true
-				elsif (self.color == "White" && (toX-fromX == -1 || toX-fromX == -2))
-					return true
+			if (self.hasMoved == false)
+				if (self.color == "Black")
+					if (toX-fromX == 1)
+						return true
+					elsif (toX-fromX == 2)
+						@enPassant = true
+						return true
+					end
+				elsif (self.color == "White")
+					if (toX-fromX == -1)
+						return true
+					elsif (toX-fromX == -2)
+						@enPassant = true
+						return true
+					end
 				else
 					return false
 				end
@@ -109,4 +119,54 @@ class Pawn < Piece
 		# removes emtpy arrays that exist thanks to movePath
 		@path.delete([])
 	end
+
+	def checkPromotion
+		pieces = ["rook","knight","bishop","queen"]
+
+		if (self.color == "Black")
+			# get x coord of position, see if pawn has made it to the other side
+			if (self.position/8 == 7)
+				puts "What piece do you want to promote pawn to?"
+				promotion = gets.chomp
+
+				while !(pieces.include?(promotion.downcase))
+					puts "Not a valid piece. Try again"
+					promotion = gets.chomp
+				end
+				return promotion
+			end
+		else
+			if (self.position/8 == 0)
+				puts "What piece do you want to promote pawn to?"
+				promotion = gets.chomp
+
+				while !(pieces.include?(promotion.downcase))
+					puts "Not a valid piece. Try again"
+					promotion = gets.chomp
+				end
+				return promotion
+			end
+		end
+		return nil
+	end
+
+	def getEnPassantSquare
+		if (@enPassant == true)
+			if (self.color == "Black")
+				# don't need to worry about bounds checking, since enPassant will only be true on the intial move, and +-10 won't be offboard
+				return self.position-8
+			else
+				return self.position+8
+			end
+		else
+			return nil
+		end
+	end
 end
+
+# en passant
+# can only capture en passant on the very next turn, and yes, it can only be opposing pawns
+
+# have a function that returns true when a pawn moves 2 squares, and have game check for that in the game loop
+# get lastMove, and if it's a pawn (and enPassant is true), then see if that pawn can capture en passant
+# which means that the en passant square would be on the same horizontal as lastMove
